06f44718b0a7a8bf623c18065aebd91c3f85a92a
Max <johndoe@example.com>
2025-02-21 09:25:09 +0300

    even num

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 
 brain-games:
 	node bin/brain-games.js
+
+brain-even:
+	node bin/brain-even.js
---
+++ bin/brain-even.js
@@ -0,0 +1,38 @@
+// Код первой игры
+
+import { helloUser, askName } from '../src/cli.js'
+
+import readlineSync from 'readline-sync'
+
+
+const isEven = (number) => number % 2 === 0
+
+const gameEven = () => {
+  helloUser()
+  const userName = askName()
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+  let correctAnswers = 0
+  const rounds = 3
+
+  for (let i = 0; i < rounds; i++) {
+    const randomNumber = Math.floor(Math.random() * 100)
+    console.log(`Question: ${randomNumber}`)
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase()
+
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no'
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!')
+      correctAnswers += 1
+    } else {
+      console.log(`'${userAnswer}' is wrong answer; (Correct answer was '${correctAnswer}').`)
+      console.log(`Let's try again, ${userName}!`)
+      return
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`)
+}
+
+gameEven()
